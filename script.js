caf9092824c2870496406e5b277359087f50bfaa
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-01-16 22:50:31 -0500

    determining password length

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ var lowercaseArray = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T
 var numbersArray = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9,";
 var specialCharactersArray = ('@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.');
 
+function choosePasswordLength() {
+  var promptLength = window.prompt("Choose a password length between 8 to 128 characters.");
+  
+  if ((promptLength > 128) || (promptLength < 8)) {
+    window.alert("Password must be AT LEAST 8 characters and no more than 128 characters!");
+    return choosePasswordLength();
+  } else {
+    window.alert("Choose whether or not you would like to include lowercase letters in your password.");
+    return promptLength;
+  }
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
