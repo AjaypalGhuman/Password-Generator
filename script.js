3fe75a9c8814ad4391839f3df4d054937f3e67ea
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-01-16 22:23:22 -0500

    Array creation for password characters

--- script.js
+++ script.js
@@ -4,6 +4,11 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var uppercaseArray = "a,b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+var lowercaseArray = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+var numbersArray = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9,";
+var specialCharactersArray = ('@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.');
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
