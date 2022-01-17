bfb6eabfe28b1765e609ee5f8846ab60633a338b
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-01-16 23:07:45 -0500

    updated readme file

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 A password generating website. 
 
 The user generates the password by clicking the button and then has to follow through certain prompts to make sure that the password that is generated meets a certain criteria. The end results in a randomly generated password. 
+
+
+file:///Users/ajaypalghuman/Desktop/projects/Password-Generator/Password-Generator/index.html
--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var uppercaseArray = "a,b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t,
 var lowercaseArray = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
 var numbersArray = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9,";
 var specialCharactersArray = ('@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.');
-var promptLength;
+
 
 function choosePasswordLength() {
   var promptLength = window.prompt("Choose a password length between 8 to 128 characters.");
