8f0d5c614bc31f88d8a1d087727b0fd98fec6e4f
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-01-25 21:15:23 -0500

    added if statements for the password generating aspect of the function with true and false values

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var characterLength = 8;
+var choiceArray = [];
 var lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
@@ -27,10 +28,26 @@ function generatePassword() {
 }
 
 function getPrompts() {
+  choiceArray = [];
+  
   characterLength = parseInt(prompt("How many characters would you like your password to be? Choose between 8 to 128 characters."));
 
-  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
     alert("Character length must be between 8 to 128 characters. Please put a valid response.");
     return false;
   }
+
+  if (confirm("Would you like uppercase letters in your password?")) {
+    choiceArray = choiceArray.concat(uppercaseArray);
+  }
+  if (confirm("Would you like lowercase letters in your password?")) {
+    choiceArray = choiceArray.concat(lowercaseArray);
+  }
+  if (confirm("Would you like numbers in your password?")) {
+    choiceArray = choiceArray.concat(numbersArray);
+  }
+  if (confirm("Would you like special characters in your password?")) {
+    choiceArray = choiceArray.concat(specialCharactersArray);
+  }
+  return true;
 }
