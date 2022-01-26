2423de9a4be1c2ead11dd01b9c7e22e756ce3e78
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-01-25 20:24:35 -0500

    added arrays to script.js file

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Assignment code here
+var characterLength = 8;
+var lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
+var specialCharactersArray = ['@', '%', '+', '/', '!', '#', '$', '^', '?', ':', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-var uppercaseArray = "a,b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
-var lowercaseArray = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
-var numbersArray = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9,";
-var specialCharactersArray = ('@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.');
 
 
 function choosePasswordLength() {
@@ -21,6 +21,12 @@ function choosePasswordLength() {
     return promptLength;
   }
 }
+function chooseLowercase(){
+  var promptLowercase = window.prompt("Do you wish to include lowercase letters in your password?");
+}  
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -31,5 +37,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {}
